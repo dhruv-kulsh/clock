06a816d030c05351e9c9d5282f83ccf75c580c10
dhruv-kulsh <93292796+dhruv-kulsh@users.noreply.github.com>
2021-12-27 15:46:29 +0530

    Initial files uploaded

---
+++ index.js
@@ -0,0 +1,19 @@
+setInterval(() => {
+    let date = new Date();
+    let htime = date.getHours();
+    let mtime = date.getMinutes();
+    let stime = date.getSeconds();
+
+    let hrotation = 30 * htime + mtime / 2;
+    let mrotation = 6 * mtime;
+    let srotation = 6 * stime
+
+    hour = document.querySelector('#hour')
+    minute = document.querySelector('#minute')
+    second = document.querySelector('#second')
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
